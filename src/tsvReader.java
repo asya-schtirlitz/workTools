578432976e1f29e06cb45e6ffd6d9e457a9a0cc3
Маланчева Анастасия Алексеевна <amalancheva@cmx.ru>
2016-12-20 19:18:09 +0300

    Initial commit

---
+++ src/tsvReader.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class tsvReader{
+    public static void main(String args[]) throws Exception{
+        BufferedReader tsvFile = new BufferedReader(new FileReader("C:\\Users\\amalancheva\\Documents\\temp\\JAVA\\test.tsv"));
+        String dataRow = tsvFile.readLine();
+
+        while (dataRow != null){
+            List<String> list = new ArrayList<String>();
+            String[] dataArray = dataRow.split("\t");
+            for (String item:dataArray){
+                list.add(item);
+            }
+            Iterator<String> it = list.iterator();
+            while (it.hasNext()){
+                String txt = it.next();
+                System.out.print(txt + ", ");
+            }
+            System.out.println();
+            dataRow = tsvFile.readLine();
+        }
+        tsvFile.close();
+        System.out.println();
+    }
+}
